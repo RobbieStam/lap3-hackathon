a8c09c1b383814a5902e3f8e181d7907c0f1b38c
Robbie Stamp <stamprobbie@gmail.com>
2023-09-07 10:41:37 +0100

    adding NavLinks to Search and superheros page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { NavBar } from './layout'
+import * as Pages from './pages'
 
 const App = () => {
   return (
     <Routes>
       <Route path="/" element={<NavBar />}>
-      
+        <Route index element={<Pages.Home />}/>
+        <Route path="/superheros" element={<Pages.Home />}/>
+        <Route path="/search" element={<Pages.Home />}/>
       </Route>
     </Routes>
   )
